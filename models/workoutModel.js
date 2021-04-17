de492f6f9c77f268e59c601ee295788ac241ec31
Justin Hohing <jnh0627@yahoo.com>
2021-04-17 18:32:13 -0400

    Added index.js to models folder

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,42 +1,48 @@
 const mongoose = require("mongoose");
-
 const Schema = mongoose.Schema;
-
 const workoutSchema = new Schema({
-    day: { type: Date, default: () => new Date() },
-    exercises: [
-      {
+    day:{
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
         type: {
-          type: String,
+            type: String,
+            trim: true,
+            required: "Enter an Exercise Type"
         },
         name: {
-          type: String,
+            type: String,
+            trim: true,
+            required: "Enter The Exercise Name"
         },
         duration: {
-          type: Number,
+            type: Number,
+            required: "Please Enter The Duration Of The Exercise"
         },
-        weight: Number,
-        reps: Number,
-        sets: Number,
-        distance: Number,
-      },
-    ],
+        weight: {
+            type: Number,
+        },
+        reps: {
+            type: Number,
+        },
+        sets: {
+            type: Number,
+        },
+        distance: {
+            type: Number,
+        }
+    }]
+},
+{
+  toJSON: {
+    virtuals: true,
   },
-  {
-    toJSON: {
-      virtuals: true,
-    },
-  }
-);
-
+})
 workoutSchema.virtual("totalDuration").get(function () {
-  const duration = this.exercises.reduce((acc, cur) => {
-    return acc + cur.duration;
-  }, 0);
-
-  return duration;
-});
-
+    return this.exercises.reduce((total, exercise) => {
+        return total + exercise.duration
+    }, 0)
+})
 const Workout = mongoose.model("Workout", workoutSchema);
-
 module.exports = Workout;
